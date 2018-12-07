ab3b15a58510d334b1c8cbe573759cab79e8cf51
Harun-Ur-Rashid(Shimanto) <22985408+harunshimanto@users.noreply.github.com>
2018-12-07 23:13:21 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,request
 from flask_bootstrap import Bootstrap 
 import pandas as pd 
 import numpy as np 
-
+from pml import app
 # ML Packages
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.externals import joblib
